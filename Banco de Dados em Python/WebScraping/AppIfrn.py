030aae204011f2f61056b1c83d3ccba750e9729e
leonardo-fabricio <leonardo.fabricio@escolar.ifrn.edu.br>
2021-07-14 12:16:43 -0300

    alteraçoes

--- Banco de Dados em Python/WebScraping/AppIfrn.py
+++ Banco de Dados em Python/WebScraping/AppIfrn.py
@@ -1,18 +1,16 @@
-
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import os
 from time import sleep
 from datetime import datetime
 
 cidades = ['apodi','caico','canguaretama','ceara-mirim','curraisnovos','joaocamara','paudosferros','jucurutu','lajes','macau','mossoro','natalcentral','natalcidadealta','natalzonanorte']
 for x in cidades:
     url = "https://portal.ifrn.edu.br/campus/"+x
-
+    
     driver = webdriver.Chrome()
     driver.get(url)
 
     sleep(2)
+    
     title1 = driver.find_element_by_xpath('//*[@id="principal_news"]/div[1]/a/span[1]')
     title2 = driver.find_element_by_xpath('//*[@id="principal_news"]/div[2]/a/span[1]')
     title3 = driver.find_element_by_xpath('//*[@id="principal_news"]/div[3]/a/span[1]')
@@ -25,5 +23,6 @@ for x in cidades:
     
     with open("noticias.txt",'a') as arq:
         arq.write(x+' - ('+str(datetime.now())+')\n'+title1.text+'\n'+title2.text+'\n'+title3.text+'\n'+title4.text+'\n'+title5.text+'\n'+title6.text+'\n'+title7.text+'\n'+title8.text+'\n\n')
+    driver.close()
     
 print("ACABOU!")
